d9a51dea2bfbb281cc875ba05ac269d7fb4fe8e1
Vikaskumar420 <vikaskumar766820@gmail.com>
2025-08-14 16:27:50 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./Store.js";
+import { store } from "./store.js";
 import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById("root")).render(
